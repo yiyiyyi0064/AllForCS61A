585b5d490ec40e77b2b895d5a73ec759de3c7043
yiyiyyi <yiyiyyi333@gmail.com>
2024-10-16 00:42:29 +0800

    scheme+lab09+hw07(1)

--- CS61A/hw/hw07/hw07.scm
+++ CS61A/hw/hw07/hw07.scm
@@ -1,15 +1,18 @@
-(define (square n) (* n n))
-
-(define (pow base exp) 'YOUR-CODE-HERE)
-
-(define (repeatedly-cube n x)
-  (if (zero? n)
-      x
-      (let (_________________)
-        (* y y y))))
-
-(define (cddr s) (cdr (cdr s)))
-
-(define (cadr s) 'YOUR-CODE-HERE)
-
+(define (square n) (* n n))
+
+(define (pow base exp) (cond ((= exp 1) base)
+  ((even? exp) (square (pow base (/ exp 2))))
+  (else (* base (square (pow base (/ (- exp 1) 2)))) )))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)))
+      ;会一直到n=0 然后再一层一层返回到n 那是最后一次结果 然后返回
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) 'YOUR-CODE-HERE)
+
 (define (caddr s) 'YOUR-CODE-HERE)
--- CS61A/lab/lab09/lab09.scm
+++ CS61A/lab/lab09/lab09.scm
@@ -1,10 +1,18 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond ((< num1 num2) -1)
+                                        ((> num1 num2) 1)
+                                        ((= num1 num2) 0))
+                                        )
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 
+  (lambda (x) (if (> (- n 1) 0)
+  ((repeat f (- n 1))(f x))
+  (f x))
+        )
+  )
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +24,7 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) (cond ((= (modulo a b) 0) b)
+  ((= (modulo b a) 0) a)
+  ((> a b) (gcd b (modulo a b)))
+  ((< a b)  (gcd a (modulo b a)))))
