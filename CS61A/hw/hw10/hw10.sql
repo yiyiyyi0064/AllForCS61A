e1be8f4da6deed1efd6c22c713a2b6d41e6d1ec8
yiyiyyi <yiyiyyi333@gmail.com>
2024-10-22 19:01:08 +0800

    disc11+hw10

--- CS61A/hw/hw10/hw10.sql
+++ CS61A/hw/hw10/hw10.sql
@@ -26,24 +26,48 @@ CREATE TABLE sizes AS
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_parent_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.child AS name
+  FROM parents AS a, dogs AS b --如果要从两个表格中得到数据 那么需要两个object 来交叉对比
+  WHERE a.parent=b.name --只会筛选出b中有parent name的row 
+  ORDER BY b.height DESC; --在那个where之后 相当于a表有append的了与b中name相同的数据
 
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name AS dogname, b.size AS sizename
+  FROM dogs AS a, sizes AS b
+  WHERE a.height > b.min AND a.height <= b.max; --我不确定的是 这里是否指的是同一行
+  -- 有个发现 其实只要用到其中一个条件了 那么就会限定在一个row中 那么剩下所有条件都是使用这一row中的
 
 
 -- Filling out this helper table is optional
-CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE siblings AS --has the same granddad
+  SELECT a.child AS name1, c.child AS name2
+  FROM parents AS a
+  JOIN
+   parents AS b ON a.parent=b.child
+   JOIN --不用join也可以 但是要学学join
+    parents AS c ON c.parent=b.child
+  WHERE a.child <> c.child; --a.child and b.parent gandson/granddad
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT "The two siblings, " || a.name1 || " and "|| a.name2 || ", have the same size: " || b.sizename
+  FROM siblings AS a, size_of_dogs AS b, size_of_dogs AS c
+  WHERE a.name1=b.dogname AND a.name2=c.dogname AND b.sizename=c.sizename
+  AND a.name1<a.name2 --这一下使得两项一样的 有了区分 ！
+  ;
 
 
 -- Height range for each fur type where all of the heights differ by no more than 30% from the average height
 CREATE TABLE low_variance AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT fur AS nametype , MAX(height)-MIN(height) AS range 
+  FROM dogs 
+  GROUP BY fur --相同fur的会被放在一组
+  HAVING MIN(height)>=AVG(height)*0.7 AND MAX(height)<=AVG(height)*1.3
+  --having 中manipulate必须要在group by中提出 height不在fur中 只能使用聚合函数得到数据 
+  --average 直接就是整个column 的平均
+  --为什么不能用heigh而是直接用min/max 
+  --
+  ;
 
---
+++ CS61A/trials/pizza.sql
@@ -0,0 +1,12 @@
+CREATE TABLE pizzas AS
+  SELECT "Artichoke" AS name, 12 AS open, 15 AS close UNION
+  SELECT "La Val's"         , 11        , 22          UNION
+  SELECT "Sliver"           , 11        , 20          UNION
+  SELECT "Cheeseboard"      , 16        , 23          UNION
+  SELECT "Emilia's"         , 13        , 18;
+
+CREATE TABLE meals AS
+  SELECT "breakfast" AS meal, 11 AS time UNION
+  SELECT "lunch"            , 13         UNION
+  SELECT "dinner"           , 19         UNION
+  SELECT "snack"            , 22;
